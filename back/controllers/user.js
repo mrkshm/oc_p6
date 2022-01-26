2c52bedec6140354e302aeea628008837f99dea4
Markus Heumann <mrks.heumann@gmail.com>
2022-01-26 23:05:24 +0100

    injection protection on email

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -3,20 +3,25 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then(hash => {
-      // code
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ messge: "Utilisateur crée !" }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+  const email = req.body.email;
+  if (email.includes("=") || email.includes('"') || email.includes("'")) {
+    console.log("Bad characters in email.");
+  } else {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then(hash => {
+        // code
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ messge: "Utilisateur crée !" }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  }
 };
 
 exports.login = (req, res, next) => {
